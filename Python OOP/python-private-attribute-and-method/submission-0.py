4a011c9d00d65d02b805422cac8e6f9dfb5c6681
SeungTaek Lee <65439840+noelstlee@users.noreply.github.com>
2026-05-17 10:36:14 -0400

    Add: python-private-attribute-and-method - submission-0

---
+++ Python OOP/python-private-attribute-and-method/submission-0.py
@@ -0,0 +1,15 @@
+class PasswordManager:
+    def __init__(self, password: str):
+        """ Initializes the password object. """
+        self.__password = password # private attribute
+    
+    # TODO: Implement the verify_password method
+    def verify_password(self, password: str) -> bool:
+        return password == self.__password
+
+
+
+# Don't modify the code below this line
+my_password = PasswordManager("secret123")
+print(my_password.verify_password("secret123"))  # Should print: True
+print(my_password.verify_password("wrong"))      # Should print: False
