da038df5deba67ab6280b6948478acc1a1d37b5d
SeungTaek Lee <65439840+noelstlee@users.noreply.github.com>
2026-05-20 15:41:19 -0400

    Add: python-list-initialization - submission-0

---
+++ Python Coding Interviews/python-list-initialization/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def create_list_with_value(size: int, index: int, value: int) -> List[int]:
+    res = []
+    for _ in range(size):
+        res.append(0)
+    res[index] = value
+    return res
+
+
+
+# do not modify below this line
+print(create_list_with_value(5, 3, 7))
+print(create_list_with_value(1, 0, 5))
+print(create_list_with_value(10, 9, 9))
+print(create_list_with_value(10, 9, 0))
