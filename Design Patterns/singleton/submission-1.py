69753799da5599dbf4588b3d218fcaa3e798b6cd
SeungTaek Lee <65439840+noelstlee@users.noreply.github.com>
2026-05-25 08:06:29 -0400

    Add: singleton - submission-1

---
+++ Design Patterns/singleton/submission-1.py
@@ -0,0 +1,22 @@
+import threading
+
+class Singleton:
+    _instance_lock = threading.Lock()
+    _unique_instance = None
+
+    # In python consider this method as the 'getInstance'
+    def __new__(cls):
+        with cls._instance_lock:
+            if cls._unique_instance is None:
+                cls._unique_instance = super().__new__(cls)
+                # cls._unique_instance._init_value()
+            return cls._unique_instance
+
+    def _init_value(self):
+        self.value = None
+
+    def getValue(self) -> str:
+        return self.value
+
+    def setValue(self, value: str):
+        self.value = value
