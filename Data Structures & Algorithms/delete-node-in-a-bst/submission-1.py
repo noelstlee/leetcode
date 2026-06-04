d53123c5ba22c166b200cf56bb699c9854659aaf
SeungTaek Lee <65439840+noelstlee@users.noreply.github.com>
2026-06-04 18:12:51 -0400

    Add: delete-node-in-a-bst - submission-1

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minValueNode(self, root):
+        curr = root
+        while curr and curr.left:
+            curr = curr.left
+        return curr
+
+    def maxValueNode(self, root):
+        curr = root
+        while curr and curr.right:
+            curr = curr.right
+        return curr
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else: # when found
+            if not root.left: # connect the root with the right subtree
+                return root.right
+            elif not root.right: # connect the root with the left subtree
+                return root.left 
+            else: 
+                # minNode = self.minValueNode(root.right)
+                maxNode = self.maxValueNode(root.left)
+                # substitute
+                # root.val = minNode.val
+                root.val = maxNode.val
+                # root.right = self.deleteNode(root.right, root.val)
+                root.left = self.deleteNode(root.left, root.val)
+        return root
