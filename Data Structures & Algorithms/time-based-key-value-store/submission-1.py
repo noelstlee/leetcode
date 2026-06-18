b17402e99c44e5edb19c3040d8eff1765a5a73a4
SeungTaek Lee <65439840+noelstlee@users.noreply.github.com>
2026-06-18 13:40:30 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,22 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timeMap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timeMap[key].append((value, timestamp))
+        
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        feelings = self.timeMap[key]
+
+        for i in range(len(feelings) - 1, -1, -1):
+            if feelings[i][1] <= timestamp:
+                return feelings[i][0]
+        
+        return ""
+
+        
+        
+        
